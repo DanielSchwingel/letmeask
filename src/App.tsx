d835c05d898db84cc9f748a833fb4196ef873b8e
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2021-06-24 11:45:48 -0300

    Second class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/authContext';
 
 function App() {
 	return (
-		<Home/>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Route path='/' exact component={Home}/>
+				<Route path='/rooms/new' component={NewRoom}/>
+			</AuthContextProvider>
+		</BrowserRouter>
 	);
 }
 
